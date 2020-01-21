611775552a28cffe99dcce683f3e15ca7fdfae08
Guilherme Cintra <cinguilherme@gmail.com>
2020-01-21 08:52:45 -0300

    fixed port mapping problem with postgres, next up is to auto create the tables with sqlalchemy

--- code/app.py
+++ code/app.py
@@ -26,6 +26,7 @@ POSTGRES_URL = get_env_variable("POSTGRES_URL")
 POSTGRES_USER = get_env_variable("POSTGRES_USER")
 POSTGRES_PW = get_env_variable("POSTGRES_PW")
 POSTGRES_DB = get_env_variable("POSTGRES_DB")
+
 DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
 
 #DBs URIs
