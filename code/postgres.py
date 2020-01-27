90aa42d207403ecbd6e1a8a34f3ed59535ba3e12
Cintra, Guilherme <cinguilherme@gmail.com>
2020-01-26 23:04:41 -0300

    change dburl env to match heroku standarts

--- code/postgres.py
+++ code/postgres.py
@@ -8,13 +8,8 @@ def get_env_variable(name):
         raise Exception(message)
 
 # the values of those depend on your setup
-POSTGRES_URL = get_env_variable("POSTGRES_URL")
-POSTGRES_USER = get_env_variable("POSTGRES_USER")
-POSTGRES_PW = get_env_variable("POSTGRES_PW")
-POSTGRES_DB = get_env_variable("POSTGRES_DB")
-POSTGRES_URL = os.environ['POSTGRES_URL']
 
-DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+DATABASE_URL = os.environ['DATABASE_URL']
 
 def get_postgres_uri():
-    return DB_URL
+    return DATABASE_URL
